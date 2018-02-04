b49fc90382fb37539fd2a72acd335e18d08cc737
Jon Ribeiro <contact@jonathas.com>
2018-02-04 21:47:55 +0100

    Implemented code to consume an API

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
+import {HttpModule} from '@angular/http';
+import 'rxjs/add/operator/map'; // Adds a map to an observable
 
 @NgModule({
     imports: [
-        BrowserModule, FotoModule
+        BrowserModule, FotoModule, HttpModule
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
