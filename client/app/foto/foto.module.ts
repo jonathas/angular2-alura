f209276a2b91574e82aef514e0043c2479d6f21f
Jon Ribeiro <contact@jonathas.com>
2018-02-05 22:56:04 +0100

    Implemented a pipe

--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FotoComponent } from './foto.component';
+import { FiltroPorTitulo } from './foto.pipes';
 
-@NgModule({ declarations: [FotoComponent], exports: [FotoComponent] })
+@NgModule({ declarations: [FotoComponent, FiltroPorTitulo], exports: [FotoComponent, FiltroPorTitulo] })
 export class FotoModule { }
